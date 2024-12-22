966694775bcf6d13beeb06985540935f10612318
Paulo Pitte <pauloanalista@outlook.com>
2024-12-22 13:39:05 -0300

    upcode

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+
+public record CreateProductCommand(string name, List<string> category, string description, string imageFile, decimal price)
+    : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid id);
+
+
+
+
+internal sealed class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+

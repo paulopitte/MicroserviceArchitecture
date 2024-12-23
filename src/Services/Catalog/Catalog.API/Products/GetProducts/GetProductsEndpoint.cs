f2d4e21091a7f8ce766725639fc0e15390c1216a
Paulo Pitte <pauloanalista@outlook.com>
2024-12-23 18:43:32 -0300

    upcode

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+using Mapster;
+
+namespace Catalog.API.Products.GetProducts;
+
+
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+         app.MapGet("/products", async (ISender sender) =>
+         {
+             var result = await sender.Send(new GetProductsQuery());
+
+             var products = app.Adapt<GetProductsResponse>();
+
+             return Results.Ok(products); 
+         }).WithName("GetProducts")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductsQueryHandler.Handler called with {query}");
+
+        var response = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(response);
+    }
+}
+

9920a74a3ccfab0d10d0eeddf12d5d1179bb2bfa
Paulo Pitte <pauloanalista@outlook.com>
2024-12-26 19:47:54 -0300

    upcode

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal sealed class GetProductByCategoryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryHandler.Handler called with {@request}");
+
+        var products = await session.Query<Product>()
+            .Where(c => c.Category.Contains(request.Category))
+            .ToListAsync();
+
+        return new GetProductByCategoryResult(products);
+    }
+}
+

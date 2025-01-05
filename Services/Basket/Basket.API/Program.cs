363ade350f8592df06e890a3756cd0666a20fcff
Paulo Pitte <pauloanalista@outlook.com>
2025-01-05 10:55:56 -0300

    add Basket api project

---
+++ Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,33 @@
+using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.Security;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionName = Security.GetConnectionString(builder.Configuration.GetConnectionString("Database")!);
+
+
+// Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(connectionName!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.Run();
